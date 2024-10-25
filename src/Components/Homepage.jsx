01319a4bf6cf2905ac08ae16e9fff7aedc41a1f7
KevinRichard1 <kevin.rohit@gmail.com>
2024-10-25 17:46:49 -0400

    Inital frontend commit

---
+++ src/Components/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div className='homepage' class="backgroun">
+        <h1 class>Process information from calls </h1>
+    </div>
+  )
+}
+
+export default Homepage
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './App.{html,js}',
+  ],
+  theme: {
+    colors: {
+    },
+    fontFamily: {
+
+    },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
+

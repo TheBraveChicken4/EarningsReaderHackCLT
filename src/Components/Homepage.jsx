0ea32028fe37ba05f03558bfed8f42a61bc37700
KevinRichard1 <kevin.rohit@gmail.com>
2024-10-25 19:02:20 -0400

    Added content on homepage

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import {React, useState} from 'react'
 
 const Homepage = () => {
+    function searchData(formData){
+        const queryLink = formData.get("query");
+        alert(`You searched for '${queryLink}'`);
+    }
   return (
-    <div className='homepage' class="backgroun">
-        <h1 class>Process information from calls </h1>
+    <div className='homepage' class="text-center">
+        <h1 class="text-[10vh]">Call Report</h1>
+        <p>Enter a URL below to collect information from the earnings call.</p>
+        <form action={searchData} class="mt-[1vh]">
+            <input type="url" name="url" id="url" placeholder="Enter URL" class="border-2 border-black-100/10 "/>
+            <button type="submit" value="Submit" class="bg-[black] text-[white]">Search</button>
+        </form>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './App.{html,js}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     colors: {

0ad8f89ceb3b4faf44fe9d5f27ef94a08f5f4323
KevinRichard1 <kevin.rohit@gmail.com>
2024-10-25 20:17:56 -0400

    Updated homepage to be able to send data to backend

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,17 +1,47 @@
 import {React, useState} from 'react'
 
 const Homepage = () => {
-    function searchData(formData){
-        const queryLink = formData.get("query");
-        alert(`You searched for '${queryLink}'`);
+    const [url, setUrl] = useState(``);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const urlData = {url}
+        console.log('Sending URl: ', urlData);
+
+        try{
+            const response = await fetch('http://localhost:3000/write-url', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({url}),
+            });
+
+            if (response.ok) {
+                await fetch('/run-script');
+            } else {
+                console.error('Error writing URL to file');
+            }
+        } catch(error){
+            console.error('Fetch error:', error);
+        }
     }
   return (
     <div className='homepage' class="text-center">
         <h1 class="text-[10vh]">Call Report</h1>
         <p>Enter a URL below to collect information from the earnings call.</p>
-        <form action={searchData} class="mt-[1vh]">
-            <input type="url" name="url" id="url" placeholder="Enter URL" class="border-2 border-black-100/10 "/>
-            <button type="submit" value="Submit" class="bg-[black] text-[white]">Search</button>
+        <form onSubmit={handleSubmit} class="mt-[1vh]">
+            <input 
+                type="url" 
+                name="url" 
+                id="url" 
+                placeholder="Enter URL" 
+                class="border-2 border-black-100/10 "
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+            />
+            <button type="submit" class="bg-[black] text-[white]">Search</button>
         </form>
     </div>
   )
